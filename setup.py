9c5f3380b05437ec25e367cedd8f9d5f18f989de
ti2 <lagarde.pa@gmail.com>
2019-04-08 12:03:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "pantilt"
+plugin_identifier = "pantilt_ti2"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_pantilt"
+plugin_package = "octoprint_pantilt_ti2"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-PanTilt"
+plugin_name = "OctoPrint-PanTilt_ti2"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.1.0"
@@ -27,7 +27,7 @@ plugin_author = "Marc Hannappel"
 plugin_author_email = "salandora@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/Salandora/OctoPrint-PanTilt"
+plugin_url = "https://github.com/ti2/OctoPrint-PanTilt"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
