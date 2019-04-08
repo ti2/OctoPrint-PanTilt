12a68d1f2fcb8288a1196e8218f8071445976727
ti2 <lagarde.pa@gmail.com>
2019-04-08 12:10:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ plugin_package = "octoprint_pantilt_ti2"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-PanTilt"
+plugin_name = "OctoPrint-PanTilt_ti2"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.1.0"
